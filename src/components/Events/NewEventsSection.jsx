c999d92bd79ec262449a9a388fd5e43535e94d0f
Bishesh Shrestha <bisheshshrestha7@gmail.com>
2024-03-07 21:32:06 +0100

    second commit

---
+++ src/components/Events/NewEventsSection.jsx
@@ -0,0 +1,57 @@
+import { useQuery } from "@tanstack/react-query";
+
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
+import { fetchEvents } from "../../util/http.js";
+
+export default function NewEventsSection() {
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", {max: 3}],
+    queryFn: ({signal}) => fetchEvents({signal, max: 3}),
+    staleTime: 5000,
+    // gcTime: 30000,
+  });
+  // data is the actual data we get back
+  // isPending lets us know if the data is still on it's way or it's
+  // already there
+  // isError lets us know if there is an error in our request
+  // it works ony if our code for fetching throws an error
+  // the error property contains the error message
+
+  let content;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch events"}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <section className="content-section" id="new-events-section">
+      <header>
+        <h2>Recently added events</h2>
+      </header>
+      {content}
+    </section>
+  );
+}
